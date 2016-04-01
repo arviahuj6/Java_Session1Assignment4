366fec0451816c75149c95a469d85cf6c551b22b
arviahuj6 <geetika.arvind@gmail.com>
2016-04-01 14:04:47 +0530

    Added files via upload

---
+++ ass4.java
@@ -0,0 +1,21 @@
+package Variable;
+
+public class ass4 {
+	public static void main(String[] args) {
+		// Variable + Float
+		int x, y, z, answer, a, b, c, d;
+		x = 5;
+		y = 10;
+		z = 2;
+		a = x + y * z;
+		b = x - y + z;
+		c = (x + y)*z;
+		d = y%x;
+		System.out.println("Answer=" + a);
+		System.out.println("Answer=" + b);
+		System.out.println("Answer=" + c);
+		System.out.println("Answer=" + d);
+	}
+
+
+}
